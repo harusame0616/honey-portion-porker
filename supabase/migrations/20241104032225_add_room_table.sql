543fc06db8f37c66c350f20f6cf33c460012e6ed
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2024-11-04 17:56:58 +0900

    マイグレーションファイル修正

--- supabase/migrations/20241104032225_add_room_table.sql
+++ supabase/migrations/20241104032225_add_room_table.sql
@@ -1,7 +1,7 @@
 create schema if not exists "honey_portion_porker";
 create table "honey_portion_porker"."room" (
     "roomId" uuid not null default gen_random_uuid(),
-    "onwerRoomId" uuid not null default gen_random_uuid(),
+    "ownerRoomId" uuid not null default gen_random_uuid(),
     "memberRoomId" uuid not null default gen_random_uuid(),
     "note" text not null default ''::text,
     "updatedAt" timestamp with time zone default now(),
@@ -9,14 +9,14 @@ create table "honey_portion_porker"."room" (
 );
 alter table "honey_portion_porker"."room" enable row level security;
 CREATE UNIQUE INDEX "room_memberRoomId_key" ON honey_portion_porker.room USING btree ("memberRoomId");
-CREATE UNIQUE INDEX "room_onwerRoomId_key" ON honey_portion_porker.room USING btree ("onwerRoomId");
+CREATE UNIQUE INDEX "room_ownerRoomId_key" ON honey_portion_porker.room USING btree ("ownerRoomId");
 CREATE UNIQUE INDEX room_pkey ON honey_portion_porker.room USING btree ("roomId");
 alter table "honey_portion_porker"."room"
 add constraint "room_pkey" PRIMARY KEY using index "room_pkey";
 alter table "honey_portion_porker"."room"
 add constraint "room_memberRoomId_key" UNIQUE using index "room_memberRoomId_key";
 alter table "honey_portion_porker"."room"
-add constraint "room_onwerRoomId_key" UNIQUE using index "room_onwerRoomId_key";
+add constraint "room_ownerRoomId_key" UNIQUE using index "room_ownerRoomId_key";
 grant usage on schema "honey_portion_porker" to postgres,
     anon,
     authenticated,
