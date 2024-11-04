e89d705394aeebac0f34da1efc047f817355a6cb
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2024-11-04 17:36:20 +0900

    add_new_room_creation

---
+++ web/src/app/new-room-creation-form.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { ReloadIcon } from "@radix-ui/react-icons";
+import Form from "next/form";
+import { useActionState } from "react";
+import { createNewRoomAction } from "./_actions/create-new-room-action";
+
+export function NewRoomCreationForm() {
+  const [state, formAction, isPending] = useActionState(
+    createNewRoomAction,
+    undefined
+  );
+  return (
+    <Form action={formAction}>
+      <Button disabled={isPending} className="w-40">
+        {isPending ? (
+          <ReloadIcon
+            className="animate-spin"
+            role="img"
+            aria-label="Creating new room"
+          />
+        ) : (
+          "Create new room"
+        )}
+      </Button>
+      <div>{state?.message}</div>
+    </Form>
+  );
+}
