db73d5581d4a0f21eff5b529f3627cf6deb680c6
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2024-11-07 07:09:39 +0900

    robot.txt

---
+++ web/src/app/robot.ts
@@ -0,0 +1,12 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: "/rooms/",
+    },
+    sitemap: `https://${process.env.VERCEL_URL}/sitemap.xml`,
+  };
+}
