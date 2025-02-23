9d33c7369026adc1542b253d913179f9d7323df3
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2025-02-23 11:31:14 +0900

    new-room-creation

--- packages/web/src/app/_actions/create-new-room-action.ts
+++ packages/web/src/app/_actions/create-new-room-action.ts
@@ -1,42 +1,39 @@
 "use server";
 
-import { randomUUID } from "node:crypto";
 import { createClient } from "@/lib/supabase/server";
-import { redirect } from "next/navigation";
+import { randomUUID } from "node:crypto";
 
-type ActionReturn = Promise<{ success: false; message: string } | undefined>;
-
-async function createNewRoom(): Promise<
+export async function createNewRoomAction(): Promise<
 	| {
 			success: true;
-			data: { roomId: string; ownerRoomId: string; memberRoomId: string };
+			data: { ownerRoomId: string; memberRoomId: string };
 	  }
 	| { success: false; message: string }
 > {
 	const client = await createClient();
 
-	const newRoom = {
-		roomId: randomUUID(),
-		ownerRoomId: randomUUID(),
-		memberRoomId: randomUUID(),
-	};
+	const ownerRoomId = randomUUID();
+	const memberRoomId = randomUUID();
 
-	const result = await client.from("room").insert(newRoom);
+	try {
+		const result = await client.from("room").insert({
+			roomId: randomUUID(),
+			ownerRoomId,
+			memberRoomId,
+		});
 
-	if (result.error) {
+		if (result.error) {
+			return {
+				success: false,
+				message: "ルームの作成に失敗しました。",
+			};
+		}
+
+		return { success: true, data: { ownerRoomId, memberRoomId } };
+	} catch (error) {
 		return {
 			success: false,
 			message: "ルームの作成に失敗しました。",
 		};
 	}
-
-	return { success: true, data: newRoom };
-}
-
-export async function createNewRoomAction(): ActionReturn {
-	const newRoomCreationResult = await createNewRoom();
-
-	return newRoomCreationResult.success
-		? redirect(`/rooms/${newRoomCreationResult.data.ownerRoomId}`)
-		: { success: false, message: newRoomCreationResult.message };
 }
