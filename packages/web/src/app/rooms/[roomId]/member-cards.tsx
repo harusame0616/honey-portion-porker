2ee5ea687169bd31c8deb4bcb9b2c0e99fb26a2a
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2025-03-14 02:31:45 +0900

    MemberCard コンポーネントに分離

--- packages/web/src/app/rooms/[roomId]/member-cards.tsx
+++ packages/web/src/app/rooms/[roomId]/member-cards.tsx
@@ -1,6 +1,27 @@
 import { ChevronUpIcon } from "lucide-react";
 import { Card } from "./card";
 
+type MemberCardProps = {
+	number?: number;
+	isOpen: boolean;
+	isMine: boolean;
+};
+
+function MemberCard({ number, isOpen, isMine }: MemberCardProps) {
+	return (
+		<div className="flex flex-col items-center relative">
+			<Card isOpen={isOpen} selected={!!number}>
+				{number}
+			</Card>
+			{isMine && (
+				<div className="-ml-0.5 -mt-1.5 -mb-4">
+					<ChevronUpIcon role="img" aria-label="you" />
+				</div>
+			)}
+		</div>
+	);
+}
+
 type Props = {
 	users: {
 		userId: string;
@@ -9,32 +30,24 @@ type Props = {
 	isOpen: boolean;
 	userId: string;
 };
+
 export function MemberCards({ users, isOpen, userId }: Props) {
-	console.log({ userId });
 	return (
 		<ul className="flex gap-4 flex-wrap">
 			{users.length ? (
-				users.map((user) => (
-					<li key={user.userId}>
-						<div className="flex flex-col items-center relative">
-							<Card isOpen={isOpen} selected={!!user.card} key={user.userId}>
-								{user.card}
-							</Card>
-							{user.userId === userId && (
-								<div className="-ml-0.5 -mt-1.5 -mb-4">
-									<ChevronUpIcon role="img" aria-label="you" />
-								</div>
-							)}
-						</div>
+				users.map(({ userId: memberId, card }) => (
+					<li key={memberId}>
+						<MemberCard
+							number={card}
+							isOpen={isOpen}
+							isMine={memberId === userId}
+						/>
 					</li>
 				))
 			) : (
-				<div className="flex flex-col items-center relative">
-					<Card isOpen={false} selected={false} key={userId} />
-					<div className="-ml-0.5 -mt-1.5 -mb-4">
-						<ChevronUpIcon role="img" aria-label="you" />
-					</div>
-				</div>
+				<li>
+					<MemberCard isOpen={false} isMine />
+				</li>
 			)}
 		</ul>
 	);
