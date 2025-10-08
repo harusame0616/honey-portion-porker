802f702ace69af29dfead42c80022c5560792d89
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2025-10-09 06:31:15 +0900

    Remove mobile device configurations from Playwright test setup

--- packages/e2e/playwright.config.ts
+++ packages/e2e/playwright.config.ts
@@ -35,9 +35,6 @@ export default defineConfig({
 	projects: [
 		{ name: "chromium", use: { ...devices["Desktop Chrome"] } },
 		{ name: "webkit", use: { ...devices["Desktop Safari"] } },
-
-		{ name: "Mobile Chrome", use: { ...devices["Pixel 5"] } },
-		{ name: "Mobile Safari", use: { ...devices["iPhone 12"] } },
 	],
 
 	/* Run your local dev server before starting the tests */
