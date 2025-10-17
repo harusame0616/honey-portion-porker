9e09872313911874514240f72d2f368871c7ff1f
Nemoto Masaharu <57859567+harusame0616@users.noreply.github.com>
2025-10-17 23:57:56 +0900

    CI エラー修正 (#221)
    * テストコミット
    
    * Remove unnecessary database reset step from e2e test workflow and simplify web server command in Playwright configuration.
    
    * Revert "テストコミット"
    
    This reverts commit 6ea11780d1ce1ea3179cb6b137ab05687a6aed1f.

--- packages/e2e/playwright.config.ts
+++ packages/e2e/playwright.config.ts
@@ -40,7 +40,7 @@ export default defineConfig({
 
 	/* Run your local dev server before starting the tests */
 	webServer: {
-		command: "pnpm --filter web build && pnpm --filter web start",
+		command: "pnpm --filter web start",
 		reuseExistingServer: !process.env.CI,
 		url: "http://127.0.0.1:3000",
 	},
